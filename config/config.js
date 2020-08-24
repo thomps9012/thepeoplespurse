a419a926aef6b873366e17924382740b6b636d57
Samuel Thompson <thomps9012@gmail.com>
2020-08-24 11:04:13 -0400

    added in mysql into config

--- config/config.js
+++ config/config.js
@@ -1,12 +1,18 @@
 module.exports = {
     "development": {
+        "username": process.env.MYSQL_USER,
+        "password": process.env.MYSQL_KEY,
+        "database": process.env.MYSQL_DBNAME,
+        "host": process.env.MYSQL_HOST,
+        "dialect": "mysql",
+      },
+      "test": {
         "username": "root",
         "password": "root",
         "database": "budgetvotes",
         "host": "127.0.0.1",
-        "port": 3306,
-        "dialect": "mysql"
-    },
+        "dialect": "mysql",
+      },
     "production": {
         "use_env_variable": "JAWSDB_URL",
         "dialect": "mysql"
