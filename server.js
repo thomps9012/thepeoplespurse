9a21ed03e200cf22ee67c116a8a7357bfe182d0b
Samuel Thompson <thomps9012@gmail.com>
2020-08-25 08:45:38 -0400

    updated server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(bodyParser.json());
 app.use(express.static('client/build'));
 
 if(process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static('client/build'));
   //
   app.get('*', (req, res) => {
     res.sendfile(path.join(__dirname = 'client/build/index.html'));
