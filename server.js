6f48e0906fba61cc1b5e2bf2b7bc14a5b56fb622
Samuel Thompson <thomps9012@gmail.com>
2020-08-25 08:48:00 -0400

    updated server.js

--- server.js
+++ server.js
@@ -12,13 +12,14 @@ var sequelize = require('sequelize');
 const votes = require('./models/votes');
 var PORT = process.env.PORT || 3001;
 
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use(express.static('client/build'));
 
 if(process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  app.use(express.static(path.join(__dirname, 'client/build')));
   //
   app.get('*', (req, res) => {
     res.sendfile(path.join(__dirname = 'client/build/index.html'));
