adba80f7415c66b10b5f8f4a406f45377466e955
Samuel Thompson <thomps9012@gmail.com>
2020-08-27 08:56:21 -0400

    reworking server

--- server.js
+++ server.js
@@ -14,17 +14,9 @@ app.use(bodyParser.json());
 
 app.use(express.static('client/build'));
 
-if(process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')));
-  //
-  app.get('*', (req, res) => {
-    res.sendfile(path.join(__dirname = 'client/build/index.html'));
-  })
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
 }
-// build mode
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/public/index.html'));
-});
 
 
 // sets up connection to db
