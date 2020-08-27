eacf009267c725b409f71625dbebf02c190b1706
Samuel Thompson <thomps9012@gmail.com>
2020-08-27 08:45:38 -0400

    updated server.js

--- server.js
+++ server.js
@@ -15,15 +15,15 @@ app.use(bodyParser.json());
 app.use(express.static('client/build'));
 
 if(process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '..', 'public')));
+  app.use(express.static(path.join(__dirname, 'client/build')));
   //
   app.get('*', (req, res) => {
-    res.sendfile(path.join(__dirname, '..', 'public'), 'index.html');
+    res.sendfile(path.join(__dirname = 'client/build/index.html'));
   })
 }
 // build mode
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname + '/client/public/index.html'));
 });
 
 
