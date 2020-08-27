b29d3645fc40bae92584727382278a157e47732c
Samuel Thompson <thomps9012@gmail.com>
2020-08-27 14:22:45 -0400

    changed port number

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var app = express();
 
 
 // Setting up port and requiring models for syncing
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3001;
 var db = require("./models");
 
 // Creating express app and configuring middleware needed for authentication
