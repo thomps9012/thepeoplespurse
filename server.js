69d19f812c5cb19e8599b619ff2c73303b6cc232
Samuel Thompson <thomps9012@gmail.com>
2020-09-03 17:48:32 -0400

    updated static build path

--- server.js
+++ server.js
@@ -14,9 +14,8 @@ var app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-}
+
 
 //connect to database
 connection.connect();
