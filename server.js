7af5755ce45126bb23b8ef8a2c4f0b1506da6e22
Samuel Thompson <thomps9012@gmail.com>
2020-09-03 18:19:00 -0400

    added in a app.get file

--- server.js
+++ server.js
@@ -18,7 +18,10 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-
+app.get("*", function (req, res) {
+  const index = path.join(__dirname, 'build', 'index.html');
+  res.sendFile(index);
+});
 
 app.use(routes)
 
