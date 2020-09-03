ea2bb7db8b6bbee03a13a8782ea9f3a1ee1300bc
Samuel Thompson <thomps9012@gmail.com>
2020-09-03 17:57:09 -0400

    updated server path

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ var app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-  app.use(express.static("client/build"));
+  app.use(express.static("./client/build"));
 
 
 //connect to database
