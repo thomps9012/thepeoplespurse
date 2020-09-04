23da4a8e8b8efa6af5d1c1e40a1a8e9bcd8e4323
Samuel Thompson <thomps9012@gmail.com>
2020-09-03 22:24:35 -0400

    added in a build folder

---
+++ client/build/precache-manifest.a1c45705c212f1d88eab7fb5788b74f8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d041e9f569b69943ecd8520f06087dd0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb88d1bda14fe57be70d",
+    "url": "/static/css/main.276d126b.chunk.css"
+  },
+  {
+    "revision": "31bc9adc209af5361cc0",
+    "url": "/static/js/2.bfabb72a.chunk.js"
+  },
+  {
+    "revision": "c46ade285a7bd9e73d4de71c9509bd70",
+    "url": "/static/js/2.bfabb72a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb88d1bda14fe57be70d",
+    "url": "/static/js/main.b10eec78.chunk.js"
+  },
+  {
+    "revision": "49b863d120326d679df7",
+    "url": "/static/js/runtime-main.2f1c77c1.js"
+  },
+  {
+    "revision": "c6df440a456ed337860e661f567c6ac6",
+    "url": "/static/media/bg-books.c6df440a.png"
+  },
+  {
+    "revision": "3c66a9cc67f3e24dcb70a9cb36d78eb6",
+    "url": "/static/media/bg-dc.3c66a9cc.png"
+  },
+  {
+    "revision": "23a80a7991471d43b609aec08f1fe79a",
+    "url": "/static/media/bg-people.23a80a79.png"
+  },
+  {
+    "revision": "52da5e9f9fb8b0bdd6d62f05730d8281",
+    "url": "/static/media/library.52da5e9f.png"
+  }
+]);
