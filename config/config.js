83ff5a0e4e189c1927e5f3dcc8f669b641ef3410
Samuel Thompson <thomps9012@gmail.com>
2020-09-04 18:03:22 -0400

    updated server config

--- config/config.js
+++ config/config.js
@@ -1,10 +1,10 @@
 module.exports = {
     "development": {
-        "username": process.env.MYSQL_USER,
-        "password": process.env.MYSQL_KEY,
-        "database": process.env.MYSQL_DBNAME,
-        "host": process.env.MYSQL_HOST,
-        "dialect": "mysql",
+      "username": "root",
+      "password": "root",
+      "database": "budgetvotes",
+      "host": "127.0.0.1",
+      "dialect": "mysql",
       },
       "test": {
         "username": "root",
