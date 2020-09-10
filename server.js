f497d9d70e71d29271ab9b4887a12ed32bdc84fb
Samuel Thompson <thomps9012@gmail.com>
2020-09-10 13:08:04 -0400

    still working on server connection issues

--- server.js
+++ server.js
@@ -1,28 +1,30 @@
 const mysql = require('mysql');
 const express = require('express');
+const bodyParser = require('body-parser');
 const routes = require("./routes");
 const db = require('./models');
 var app = express();
 
 
-// Setting up port and requiring models for syncing
-var PORT = process.env.PORT || 3001;
-var db = require("./models");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-// Creating express app and configuring middleware needed for authentication
-var app = express();
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
-
-app.get("*", function (req, res) {
-  const index = path.join(__dirname, 'build', 'index.html');
-  res.sendFile(index);
+// sets up connection to db
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database: 'budgetvotes'
 });
+
+//connect to database
+connection.connect();
 
 app.use(routes)
 
-app.listen(process.env.PORT || 3001);
+db.sequelize.sync().then(function(){
+  app.listen(3001, function(){
+    console.log('app listening on port 3001!');
+  });
+});
