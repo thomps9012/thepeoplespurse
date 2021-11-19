99de5206ee25e5a1a0b79b7c869c49b30a10f6ef
Samuel Thompson <thomps9012@gmail.com>
2021-11-19 09:44:13 -0500

    login mutation returning jwt

--- peoples-purse/src/app/login/login.component.ts
+++ peoples-purse/src/app/login/login.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Apollo, gql } from 'apollo-angular';
+
+
+const LOGIN = gql`
+mutation Mutation($input: LoginInput!) {
+  login(input: $input)
+}
+`;
 
 @Component({
   selector: 'app-login',
@@ -7,9 +15,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private apollo: Apollo) { }
 
   ngOnInit(): void {
+  }
+
+  email = '';
+  password = '';
+
+  setEmail(event: Event) {
+    this.email = (event.target as HTMLInputElement).value;
+  }
+
+  setPassword(event: Event) {
+    this.password = (event.target as HTMLInputElement).value;
+  }
+
+  login(event: Event) {
+    event.preventDefault()
+
+    this.apollo.mutate({
+      mutation: LOGIN,
+      variables: {
+        input: {
+          email: this.email,
+          password: this.password
+        }
+      }
+    }).subscribe(({ data }) => {
+      console.log('got data', data);
+    }, (error) => {
+      console.log('there was an error sending the query', error);
+    });
   }
 
 }
