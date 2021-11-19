f53c5566fc4e6ef1d48194f7e914f32938599d65
Samuel Thompson <thomps9012@gmail.com>
2021-11-19 12:49:24 -0500

    updated navbar conditional showing of profile

--- peoples-purse/src/app/navbar/navbar.component.ts
+++ peoples-purse/src/app/navbar/navbar.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Apollo, gql } from 'apollo-angular';
+
+const GET_USER = gql`
+query Query($getUserId: ID!) {
+  getUser(id: $getUserId) {
+    username
+  }
+}
+`;
 
 @Component({
   selector: 'navbar',
@@ -6,10 +15,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  data?: any
+  username?: string
+  constructor(private apollo: Apollo) { }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  logout() {
+    localStorage.removeItem('USER_ID')
+    localStorage.removeItem('USER')
+    localStorage.removeItem('AUTH_TOKEN')
+    window.location.replace('/');
   }
-
+  ngOnInit() {
+    console.log(localStorage.getItem('USER_ID'))
+    const userId = localStorage.getItem('USER_ID')
+    console.log(userId)
+    if (userId != null) {
+      this.data = this.apollo.query({
+        query: GET_USER,
+        variables: {
+          getUserId: localStorage.getItem('USER_ID')
+        }
+      }).subscribe(({ data }: any) => {
+        console.log(data)
+        console.log('got data', data.getUser.username);
+        this.username = data.getUser.username
+      }, (error) => {
+        console.log('there was an error sending the query', error);
+      });
+    }
+  }
 }
