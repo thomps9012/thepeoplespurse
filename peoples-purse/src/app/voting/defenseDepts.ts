656ace332ff356d808e04ba217bc64dc6bda9b9b
Samuel Thompson <thomps9012@gmail.com>
2021-11-21 15:33:42 -0500

    cleaned up code

--- peoples-purse/src/app/voting/defenseDepts.ts
+++ peoples-purse/src/app/voting/defenseDepts.ts
@@ -2,121 +2,101 @@ export const defenseDepts = [
     {
         "name": "Department of Agriculture",
         "percent": 1,
-        "code": 1,
-        "link": "www.somewhere.com"
+        "code": 1
     },
     {
         "name": "Department of Defense",
         "percent": 20,
-        "code": 2,
-        "link": "www.somewhere.com"
+        "code": 2
     },
     {
         "name": "Department of Energy",
         "percent": 1,
-        "code": 3,
-        "link": "www.somewhere.com"
+        "code": 3
     },
     {
         "name": "Department of Education",
         "percent": 1,
-        "code": 4,
-        "link": "www.somewhere.com"
+        "code": 4
     },
     {
         "name": "Department of Health and Human Services",
         "percent": 8,
-        "code": 5,
-        "link": "www.somewhere.com"
+        "code": 5
     },
     {
         "name": "Department of the Interior",
         "percent": 1,
-        "code": 6,
-        "link": "www.somewhere.com"
+        "code": 6
     },
     {
         "name": "Department of Labor",
         "percent": 1,
-        "code": 7,
-        "link": "www.somewhere.com"
+        "code": 7
     },
     {
         "name": "Department of State",
         "percent": 10,
-        "code": 8,
-        "link": "www.somewhere.com"
+        "code": 8
     },
     {
         "name": "Department of Transportation",
         "percent": 1,
-        "code": 9,
-        "link": "www.somewhere.com"
+        "code": 9
     },
     {
         "name": "Department of Treasury",
         "percent": 1,
-        "code": 10,
-        "link": "www.somewhere.com"
+        "code": 10
     },
     {
         "name": "Environmental Protection Agency",
         "percent": 1,
-        "code": 11,
-        "link": "www.somewhere.com"
+        "code": 11
     },
     {
         "name": "Federal Communication Comission",
         "percent": 1,
-        "code": 12,
-        "link": "www.somewhere.com"
+        "code": 12
     },
     {
         "name": "Federal Election Comission",
         "percent": 1,
-        "code": 13,
-        "link": "www.somewhere.com"
+        "code": 13
     },
     {
         "name": "Federal Trade Comission",
         "percent": 1,
-        "code": 14,
-        "link": "www.somewhere.com"
+        "code": 14
     },
     {
         "name": "Department of Housing and Urban Development",
         "percent": 1,
-        "code": 15,
-        "link": "www.somewhere.com"
+        "code": 15
     },
     {
         "name": "Social Security Administration",
         "percent": 10,
-        "code": 16,
-        "link": "www.somewhere.com"
+        "code": 16
     },
     {
         "name": "Department of Justice",
         "percent": 5,
-        "code": 17,
-        "link": "www.somewhere.com"
+        "code": 17
     },
     {
         "name": "National Aeronautics and Space Administration",
         "percent": 10,
-        "code": 18,
-        "link": "www.somewhere.com"
+        "code": 18
     },
     {
         "name": "Department of Veterans Affairs",
         "percent": 20,
-        "code": 19,
-        "link": "www.somewhere.com"
+        "code": 19
     },
     {
         "name": "Equal Opportunity Employment Commission",
         "percent": 5,
-        "code": 20,
-        "link": "www.somewhere.com"
+        "code": 20
     }
 ]
--- peoples-purse/src/app/voting/voting.component.ts
+++ peoples-purse/src/app/voting/voting.component.ts
@@ -15,41 +15,41 @@ export class VotingComponent implements OnInit {
 
   depts = depts
   constructor() { }
-  totalDept= 0
-  
+  totalDept = 0
+
   ngOnInit() {
-    for(let i=0; i< depts.length; i++) {
+    for (let i = 0; i < depts.length; i++) {
       this.totalDept += (depts[i].percent)
     }
-    
+
   }
 
   updateBudget() {
     this.totalDept = 0;
-    for(let i=1; i<= 20; i++) {
+    for (let i = 1; i <= 20; i++) {
       const number = <HTMLInputElement>document.getElementById(JSON.stringify(i))
       console.log(number.value)
       this.totalDept += JSON.parse(number.value)
     }
     let totalArea = document.getElementById("totalBudget")
     let budgetAlert = <HTMLElement>document.getElementById('budgetAlert')
-    if(this.totalDept > 100){
+    if (this.totalDept > 100) {
       totalArea?.setAttribute('style', 'background-color: red')
       let dangerAlert = document.createElement("h1")
-      dangerAlert.innerHTML+=`Woah you've used ${this.totalDept-100} percent more than your budget`;
-      budgetAlert.innerHTML="";
+      dangerAlert.innerHTML += `Woah you've used ${this.totalDept - 100} percent more than your budget`;
+      budgetAlert.innerHTML = "";
       budgetAlert?.append(dangerAlert);
-    } else if (this.totalDept === 100){
+    } else if (this.totalDept === 100) {
       totalArea?.setAttribute('style', 'background-color: green')
       let dangerAlert = document.createElement("h1")
-      dangerAlert.innerHTML+="You're perfect!";
-      budgetAlert.innerHTML="";
+      dangerAlert.innerHTML += "You're perfect!";
+      budgetAlert.innerHTML = "";
       budgetAlert?.append(dangerAlert);
     } else {
       totalArea?.setAttribute('style', 'background-color: yellow')
       let dangerAlert = document.createElement("h1")
-      dangerAlert.innerHTML+=`You still have ${100-this.totalDept} percent of your budget remaining`;
-      budgetAlert.innerHTML="";
+      dangerAlert.innerHTML += `You still have ${100 - this.totalDept} percent of your budget remaining`;
+      budgetAlert.innerHTML = "";
       budgetAlert?.append(dangerAlert);
     }
   }
@@ -59,13 +59,13 @@ export class VotingComponent implements OnInit {
     this.totalDept = 100;
     let totalArea = document.getElementById("totalBudget")
     let budgetAlert = <HTMLElement>document.getElementById('budgetAlert')
-    for(let i=1; i<=20; i++) {
+    for (let i = 1; i <= 20; i++) {
       const dept = <HTMLInputElement>document.getElementById(JSON.stringify(i))
       dept.value = '5';
       totalArea?.setAttribute('style', 'background-color: green')
       let dangerAlert = document.createElement("h1")
-      dangerAlert.innerHTML+="You're perfect!";
-      budgetAlert.innerHTML="";
+      dangerAlert.innerHTML += "You're perfect!";
+      budgetAlert.innerHTML = "";
       budgetAlert?.append(dangerAlert);
     }
   }
@@ -75,11 +75,11 @@ export class VotingComponent implements OnInit {
     let totalArea = document.getElementById("totalBudget")
     let budgetAlert = <HTMLElement>document.getElementById('budgetAlert')
     totalArea?.setAttribute('style', 'background-color: green')
-      let dangerAlert = document.createElement("h1")
-      dangerAlert.innerHTML+="You're perfect!";
-      budgetAlert.innerHTML="";
-      budgetAlert?.append(dangerAlert);
-      this.depts = eduDepts;
+    let dangerAlert = document.createElement("h1")
+    dangerAlert.innerHTML += "You're perfect!";
+    budgetAlert.innerHTML = "";
+    budgetAlert?.append(dangerAlert);
+    this.depts = eduDepts;
   }
   enviroDist(event: Event) {
     event.preventDefault()
@@ -88,23 +88,23 @@ export class VotingComponent implements OnInit {
     let totalArea = document.getElementById("totalBudget")
     let budgetAlert = <HTMLElement>document.getElementById('budgetAlert')
     totalArea?.setAttribute('style', 'background-color: green')
-      let dangerAlert = document.createElement("h1")
-      dangerAlert.innerHTML+="You're perfect!";
-      budgetAlert.innerHTML="";
-      budgetAlert?.append(dangerAlert);
-      this.depts = enviroDepts;
-    }
+    let dangerAlert = document.createElement("h1")
+    dangerAlert.innerHTML += "You're perfect!";
+    budgetAlert.innerHTML = "";
+    budgetAlert?.append(dangerAlert);
+    this.depts = enviroDepts;
+  }
   healthDist(event: Event) {
     event.preventDefault()
     this.totalDept = 100;
     let totalArea = document.getElementById("totalBudget")
     let budgetAlert = <HTMLElement>document.getElementById('budgetAlert')
     totalArea?.setAttribute('style', 'background-color: green')
-      let dangerAlert = document.createElement("h1")
-      dangerAlert.innerHTML+="You're perfect!";
-      budgetAlert.innerHTML="";
-      budgetAlert?.append(dangerAlert);
-      this.depts = healthDepts;
+    let dangerAlert = document.createElement("h1")
+    dangerAlert.innerHTML += "You're perfect!";
+    budgetAlert.innerHTML = "";
+    budgetAlert?.append(dangerAlert);
+    this.depts = healthDepts;
   }
   defenseDist(event: Event) {
     event.preventDefault()
@@ -112,20 +112,22 @@ export class VotingComponent implements OnInit {
     let totalArea = document.getElementById("totalBudget")
     let budgetAlert = <HTMLElement>document.getElementById('budgetAlert')
     totalArea?.setAttribute('style', 'background-color: green')
-      let dangerAlert = document.createElement("h1")
-      dangerAlert.innerHTML+="You're perfect!";
-      budgetAlert.innerHTML="";
-      budgetAlert?.append(dangerAlert);
-      this.depts = defenseDepts;}
+    let dangerAlert = document.createElement("h1")
+    dangerAlert.innerHTML += "You're perfect!";
+    budgetAlert.innerHTML = "";
+    budgetAlert?.append(dangerAlert);
+    this.depts = defenseDepts;
+  }
   transportDist(event: Event) {
     event.preventDefault()
     this.totalDept = 100;
     let totalArea = document.getElementById("totalBudget")
     let budgetAlert = <HTMLElement>document.getElementById('budgetAlert')
     totalArea?.setAttribute('style', 'background-color: green')
-      let dangerAlert = document.createElement("h1")
-      dangerAlert.innerHTML+="You're perfect!";
-      budgetAlert.innerHTML="";
-      budgetAlert?.append(dangerAlert);
-      this.depts = transpoDepts;}
+    let dangerAlert = document.createElement("h1")
+    dangerAlert.innerHTML += "You're perfect!";
+    budgetAlert.innerHTML = "";
+    budgetAlert?.append(dangerAlert);
+    this.depts = transpoDepts;
+  }
 }
