b40bbc80167432807ea021e0857d0d74f34b4254
Samuel Thompson <thomps9012@gmail.com>
2021-11-23 12:38:02 -0500

    saving and aggregating all votes unweighted

--- peoples-purse/src/app/results/results.component.ts
+++ peoples-purse/src/app/results/results.component.ts
@@ -19,6 +19,7 @@ const ALL_VOTES = gql`
   styleUrls: ['./results.component.css']
 })
 export class ResultsComponent implements OnInit {
+  [x: string]: any;
 
   constructor(private apollo: Apollo) { }
   voteData?: any;
@@ -29,7 +30,33 @@ export class ResultsComponent implements OnInit {
     }).subscribe(({ data }: any) => {
 
       let voteArr = data.allVotes;
-      let finalData = [];
+      // console.log(voteArr)
+      // merge object keep properties and increment values via sum
+      // const deptTotal = {
+      //   name: 'test',
+      //   value: 0
+      // }
+      // let singleVote = voteArr[0].budget;
+      // singleVote.map((dept:any) => {
+      //   deptTotal.name = dept.name
+      //   console.log(deptTotal)
+      // })
+
+
+
+      // function aggVotes(n:number) {
+      //   let dept = detail.name;
+      //   {dept: n};
+      // }
+      // aggVotes.prototype.valueOf = () => {
+      //   return this.dept;
+      // };
+      // const aggDept = aggVotes(detail.percent)
+      // console.log(deptTotal)
+      // console.log(detail.name)
+      // console.log(detail.percent)
+
+      let finalData: { name: any; value: any; }[] = [];
       // single vote functionality
       let keyVote = voteArr[0].budget;
       for (const key of keyVote) {
@@ -37,7 +64,16 @@ export class ResultsComponent implements OnInit {
         let percent = key.percent
         finalData.push({ name: dept, value: percent })
       }
+      voteArr.map((vote: any) => {
+        let singleVote = vote.budget;
+        singleVote.map((detail: any) => {
+          finalData.map((dept: any) => {
+            dept.value += detail.percent
+          })
+        })
+      });
       this.voteData = finalData;
+      console.log(finalData)
       // merges in new data
       this.updateData = {
         series: [{
