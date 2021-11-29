cf965d684eff011f9a1b3cc1e119d07c3952d1d7
Samuel Thompson <thomps9012@gmail.com>
2021-11-29 12:59:13 -0500

    google api hitting location on address change

--- peoples-purse/src/app/information/information.component.ts
+++ peoples-purse/src/app/information/information.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DeptInfo } from './deptInfo';
 import { States } from './states'
+declare var gapi: any;
 
 @Component({
   selector: 'information',
@@ -15,21 +16,21 @@ export class InformationComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    // api call to opensecrets
+    gapi.load('client');
   }
+  
   displayDept = (event: Event) => {
     event.preventDefault();
-    const selectedDept = (event.target as HTMLSelectElement).value; 
+    const selectedDept = (event.target as HTMLSelectElement).value;
     const deptInfoDiv = document.getElementById('deptInfo');
-    if(deptInfoDiv != null) {deptInfoDiv.innerHTML = "";}
+    if (deptInfoDiv != null) { deptInfoDiv.innerHTML = ""; }
     const seal = document.createElement('img')
     const name = document.createElement('h3')
     const mission = document.createElement('p')
     const website = document.createElement('a')
 
-    for(let i=0; i < DeptInfo.length; i++){
-      if(DeptInfo[i].name === selectedDept) {
-        console.log(DeptInfo[i])
+    for (let i = 0; i < DeptInfo.length; i++) {
+      if (DeptInfo[i].name === selectedDept) {
         seal.src = DeptInfo[i].icon;
         seal.width = 200;
         seal.height = 200;
@@ -47,11 +48,34 @@ export class InformationComponent implements OnInit {
     }
   }
 
-  displayOfficial = (event: Event) => {
+  // const officialDiv = document.getElementById('official-display');
+  // if (officialDiv != null) { officialDiv.innerHTML = ""; }
+  location = '';
+  getAddress = (event: Event) => {
     event.preventDefault();
-    const selectedState = (event.target as HTMLSelectElement).value; 
-    // const deptInfoDiv = document.getElementById('deptInfo');
-    // if(deptInfoDiv != null) {deptInfoDiv.innerHTML = "";}
-    console.log(selectedState)
+    const address = (event.target as HTMLInputElement).value;
+    console.log(address)
+    this.location = address;
+    this.loadClient()
+  }
+
+  loadClient() {
+    gapi.client.init({
+      'apiKey': "AIzaSyDsCAsDVamr-9rGO6DwtlXHcZL-8Tx5oeA"
+    })
+    return gapi.client.load("https://civicinfo.googleapis.com/$discovery/rest?version=v2")
+      .then(function () { console.log("GAPI client loaded for API"); },
+        function (err: any) { console.error("Error loading GAPI client for API", err); });
+  }
+  execute(location: any) {
+    return gapi.client.civicinfo.representatives.representativeInfoByAddress({
+      "address": location,
+      "includeOffices": true
+    })
+      .then(function (response: any) {
+        // Handle the results here (response.result has the parsed body).
+        console.log("Response", response);
+      },
+        function (err: any) { console.error("Execute error", err); });
   }
 }
