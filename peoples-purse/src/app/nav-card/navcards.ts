f37b65e4f405620c65a8cd8227880b48cb675c81
Samuel Thompson <thomps9012@gmail.com>
2021-12-09 15:37:23 -0500

    updated landing page

--- peoples-purse/src/app/nav-card/navcards.ts
+++ peoples-purse/src/app/nav-card/navcards.ts
@@ -1,6 +1,12 @@
+const voteIcon = '../../assets/icon-vote.png';
+const educationIcon = '../../assets/icon-education.png';
+const budgetIcon = '../../assets/icon-data.png';
+
 export interface NavCard {
     link: string;
     title: string;
+    image: File;
+    iconAlt: string;
     text: string;
     button: string;
 }
@@ -9,18 +15,24 @@ export const navcards = [
     {
         "link": "/info",
         "title": "Information Center",
+        "image": educationIcon,
+        "iconAlt": "Education Icon",
         "text": "Learn more about governmental departments or your elected officials",
         "button": "Learn More"
     },
     {
         "link": "/voting",
         "title": "Voting",
+        "image": voteIcon,
+        "iconAlt": "Voting Icon",
         "text": "Ready to decide on your own personal budget?",
         "button": "Go Vote!"
     },
     {
         "link": "/results",
         "title": "Proposed Budget",
+        "image": budgetIcon,
+        "iconAlt": "Budget Icon",
         "text": "Want to see the budget your fellow citizens have proposed?",
         "button": "View the Budget"
     }
