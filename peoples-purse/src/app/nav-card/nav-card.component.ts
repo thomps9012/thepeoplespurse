13376dfb3a1067f547a3ee54f9eee6e7ab45cbd5
Samuel Thompson <thomps9012@gmail.com>
2021-12-15 12:24:50 -0500

    conditional rendering on landing page

--- peoples-purse/src/app/nav-card/nav-card.component.ts
+++ peoples-purse/src/app/nav-card/nav-card.component.ts
@@ -8,7 +8,7 @@ import { navcards } from './navcards';
 })
 export class NavCardComponent implements OnInit {
   navcards = navcards
-  
+  userLoggedIn = localStorage.getItem('USER_ID') || localStorage.getItem('TEACHER_ID')
   constructor() { }
 
   ngOnInit(): void {
