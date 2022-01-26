a25710dc1cce31b511a3f6e1caf77e854acc5dc8
Samuel Thompson <thomps9012@gmail.com>
2022-01-26 10:37:03 -0500

    signup component functioning

--- peoples-purse-next/pages/profile.tsx
+++ peoples-purse-next/pages/profile.tsx
@@ -1,6 +1,20 @@
+import { gql, useQuery } from '@apollo/client'
+
+const GET_USER = gql`
+query Query {
+    getUser {
+      _id
+      educator
+    }
+  }
+`;
+
 export default function ProfilePage() {
     return (
         <>
+            <button>
+                Logout
+            </button>
         </>
     )
 }
--- peoples-purse-next/pages/signUp.tsx
+++ peoples-purse-next/pages/signUp.tsx
@@ -2,6 +2,7 @@ import {
     useMutation,
     gql
 } from '@apollo/client';
+import { useState } from 'react';
 
 const SIGN_UP = gql`
 mutation Mutation($input: UserSignUpInput!) {
@@ -13,59 +14,83 @@ mutation Mutation($input: UserSignUpInput!) {
 
 export default function SignUp() {
     const [signUp, { data, loading, error }] = useMutation(SIGN_UP);
+    const [formState, setFormState] = useState({
+        first_name: '',
+        last_name: '',
+        email: '',
+        username: '',
+        password: ''
+    })
+
+    const handleChange = (e: any) => {
+        const { name, value } = e.target;
+        setFormState({
+            ...formState,
+            [name]: value,
+        });
+    };
 
     if (loading) return 'Submitting...';
     if (error) return `Submission error! ${error.message}`;
     return (
         <>
             <form
-                onSubmit={(e): any => {
+                onSubmit={async (e: any) => {
                     e.preventDefault();
-                    console.log(e.target)
-                    signUp({
+                    const signUpResponse = await signUp({
                         variables: {
                             input: {
-                                first_name: 'newter',
-                                last_name: 'tester',
-                                email: 'newts@tester.com',
-                                username: 'newts',
-                                password: 'test12345'
+                                first_name: formState.first_name,
+                                last_name: formState.last_name,
+                                email: formState.email,
+                                username: formState.username,
+                                password: formState.password
                             }
                         }
                     })
+                    const token = signUpResponse.data.signUp.token
+                    if(token){
+                        localStorage.setItem('auth_token', token)
+                        window.location.assign('/profile')
+                    }
                 }}
                 className="signupForm"
-                >
+            >
                 <h3>Personal Information</h3>
                 <label>First Name</label>
                 <input
                     type='text'
                     name='first-name'
                     id='first-name'
+                    onChange={handleChange}
                 />
                 <label>Last Name</label>
                 <input
                     type='text'
                     name='last-name'
                     id='last-name'
+                    onChange={handleChange}
                 />
                 <label>Username</label>
                 <input
                     type='text'
                     name='username'
                     id='username'
+                    onChange={handleChange}
                 />
                 <label>Email</label>
                 <input
                     type='text'
                     name='email'
                     id='email'
+                    onChange={handleChange}
                 />
                 <label>Password</label>
                 <input
                     type='text'
                     name='password'
                     id='password'
+                    onChange={handleChange}
                 />
                 <button
                     type='submit'
