8a7cadb433eaca3ae2cb431781c8189003ad0ca1
Samuel Thompson <thomps9012@gmail.com>
2022-01-26 10:03:04 -0500

    successful rendering of data

--- peoples-purse-next/pages/_app.tsx
+++ peoples-purse-next/pages/_app.tsx
@@ -30,8 +30,47 @@ client.query({
   `
 }).then(result => console.log(result))
 
+const ALL_VOTES = gql`
+query Query {
+  allVotes {
+    _id
+    budget {
+      code
+      name
+      percent
+    }
+  }
+}
+`
+function AllVotes() {
+  const { loading, error, data } = useQuery(ALL_VOTES);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+  return (
+    data.allVotes.map(({ budget, _id }: any) => (
+      <div key={_id}>
+              <h5>
+                Budget {_id}
+              </h5>
+        {budget.map(({ name, percent }: any) => {
+          return (
+            <>
+              <p>{name}</p>
+              <p>{percent}</p>
+            </>
+          )
+        })}
+      </div >
+    ))
+  )
+}
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+      <AllVotes />
+    </ApolloProvider>
+  )
 }
 
 export default MyApp
