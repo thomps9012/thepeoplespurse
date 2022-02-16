60094c0203ff6a5df16973740ad6cfba30369943
Samuel Thompson <thomps9012@gmail.com>
2022-02-16 13:15:07 -0500

    updated navbar with logo

--- peoples-purse-next/components/navbar.tsx
+++ peoples-purse-next/components/navbar.tsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
+import mixedLogo from '../public/mixedLogo.png';
+import Image from 'next/image'
 
 export default function Navbar() {
     const [userJWT, setUserJWT] = useState('')
-    
+
     useEffect(() => {
         setUserJWT(sessionStorage.getItem('auth_token') || '');
         const init = async () => {
@@ -12,16 +14,25 @@ export default function Navbar() {
         };
         init();
     }, []);
-    
+
     const logout = () => {
         sessionStorage.clear();
         window.location.replace('/')
     }
     return (
-        <div>
+        <div style={{ marginBottom: 100 }}>
             <nav className="navbar-fixed transparent z-depth-0">
                 <div className="nav-wrapper">
-                    <a href="/" className="brand-logo indigo-text text-darken-4">Logo</a>
+                    <a href="/"
+                        style={{ display: 'flex', marginTop: -80}}
+                        className="brand-logo indigo-text text-darken-4">
+                        <Image
+                            src={mixedLogo}
+                            layout="fixed"
+                            width={300}
+                            height={300}
+                        />
+                    </a>
                     <a href="#" data-target="mobile-demo" className="sidenav-trigger indigo-text text-darken-4"><i className="material-icons">menu</i></a>
                     <ul id="nav-mobile" className="right hide-on-med-and-down indigo-text">
                         {userJWT != '' ?
@@ -44,9 +55,17 @@ export default function Navbar() {
             </nav>
 
             <ul className="sidenav" id="mobile-demo">
+                <a href="/" style={{display: 'flex', justifyContent: 'center'}}>
+                    <Image
+                        src={mixedLogo}
+                        layout="fixed"
+                        width={200}
+                        height={200}
+                    />
+                </a>
                 {userJWT != '' ?
                     <>
-                        <li><a href="/profile"  className="grey-text text-darken-1"><i className='material-icons left'>account_box</i>Profile</a></li>
+                        <li><a href="/profile" className="grey-text text-darken-1"><i className='material-icons left'>account_box</i>Profile</a></li>
                         <li><a onClick={logout} className="grey-text text-darken-1"><i className='material-icons left'>logout</i>Logout</a></li>
                         <li><a href="/voting" className="grey-text text-darken-1"><i className='material-icons left'>how_to_vote</i>Voting</a></li>
                     </>
@@ -57,7 +76,7 @@ export default function Navbar() {
                     </>
                 }
                 <li><a href="/deptInformation" className="tab grey-text text-darken-1"><i className='material-icons left'>balance</i>Department Information</a></li>
-                        <li><a href="/electedOfficials" className="tab grey-text text-darken-1"><i className='material-icons left'>contact_phone</i>Learn Your Elected Officials</a></li>
+                <li><a href="/electedOfficials" className="tab grey-text text-darken-1"><i className='material-icons left'>contact_phone</i>Learn Your Elected Officials</a></li>
                 <li><a href="/budgetResults" className="grey-text text-darken-1"><i className='material-icons left'>analytics</i>Budget Results</a></li>
             </ul>
         </div>
