371d92767ea769bacee41b9bf9b3944c6d18c4d8
Samuel Thompson <thomps9012@gmail.com>
2022-02-20 15:05:18 -0500

    updated connection string to azure hosted web application

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -24,7 +24,7 @@ const authLink = setContext((_, { headers }) => {
 
 // development uri
 const httpLink = createHttpLink({
-  uri: 'https://lit-meadow-50865.herokuapp.com/',
+  uri: 'https://peoplespurseserver.azurewebsites.net/',
 })
 
 const client = new ApolloClient({
